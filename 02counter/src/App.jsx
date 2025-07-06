48f2b96ea09e12590f89ecd71fc976c64e9a7de9
Santosh Kumar <santosh358mis@gmail.com>
2025-07-06 12:17:26 +0530

    Initialize 02counter React application with Vite, including essential configurations, styles, and a basic counter component.

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(0)
+
+  const addvalue = () => {
+    if (counter >= 0) {
+      setCounter(counter + 1)
+    }
+
+  }
+  const removeValue = () => {
+    if (counter >0) {
+      setCounter(counter - 1)
+    }
+
+  }
+  return (
+    <>
+      <h1>Santosh Learn react</h1>
+        <h2>Counter App {counter}</h2>
+      <button onClick={addvalue}>add Value {counter}</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>remove Value{counter}</button>
+
+    </>
+  )
+}
+
+export default App
